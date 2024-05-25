3ba653ced579c2bb581d44ff9a5cc73ad6a2ea35
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-05-25 19:36:27 +0200

    update path

--- js/libs.js
+++ js/libs.js
@@ -112,7 +112,7 @@ function formatDate(date) {
 function logout() {
   localStorage.clear();
   alert("You are now logged out")
-  window.location.href='../index.html';
+  window.location.href='./index.html';
 }
 
 function getURL(){
